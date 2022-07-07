0e64ee7f429362634a930770bf19a6d88da8734c
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-07-07 15:34:41 +0300

    fix postCreateValidation

--- validations/validations.js
+++ validations/validations.js
@@ -19,6 +19,6 @@ export const loginValidation = [
 export const postCreateValidation = [
     body("title", "Введите заголовок статьи.").isLength({ min: 3 }).isString(),
     body("text", "Введите текст статьи.").isLength({ min: 3 }).isString(),
-    body("tags", "Неверный формате тегов.").optional().isString(),
+    body("tags", "Неверный формате тегов.").optional().isArray(),
     body("imageUrl", "Неверная ссылка на изображение.").optional().isString(),
 ];
