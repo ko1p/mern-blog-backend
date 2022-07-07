7260037f0a143d362afac689b15dcd52622a46eb
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-07-07 14:58:46 +0300

    fix getOne controller

--- controllers/postController.js
+++ controllers/postController.js
@@ -45,7 +45,7 @@ export const getOne = async (req, res) => {
 
         res.json(doc);
       }
-    );
+    ).populate('user');
   } catch (err) {
     console.log(err);
     res.status(500).json({
