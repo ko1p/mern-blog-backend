7f8c580e57d0de7cc6012251bd2e51080224f08d
Anton Belko <30528165+ko1p@users.noreply.github.com>
2022-07-08 17:15:16 +0300

    replace tags spliting from frontend to backend

--- controllers/postController.js
+++ controllers/postController.js
@@ -94,7 +94,7 @@ export const create = async (req, res) => {
       title: req.body.title,
       text: req.body.text,
       imageUrl: req.body.imageUrl,
-      tags: req.body.tags,
+      tags: req.body.tags.split(', '),
       user: req.userId,
     });
 
